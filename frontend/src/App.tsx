77eb76aa33fdf88bbd77564193daf1c9e0dc45be
Ronald Jabouin <rj336@cornell.edu>
2026-03-23 20:25:49 -0400

    use ref

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import "./App.css";
 import Chat from "./Chat";
 import Logo from "./components/Logo";
